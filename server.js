c442a30313eb462590aff849c0eb3f74c50d180c
Ankit-Ajmera <ankit.ajmera3@gmail.com>
2018-10-09 11:40:17 +0400

    Update server.js

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ var server = http.createServer(function (req, res) {
 		res.write('\n');
 		res.write('Host: ' + os.hostname() + '\n');
 		res.write('OssssssssS Type: ' + os.type() + '\n');
-		res.write('OS Platform: ' + os.platform() + '\n');
+		res.write('O222S Platform: ' + os.platform() + '\n');
 		res.write('OS Arch: ' + os.arch() + '\n');
 		res.write('OS Release: ' + os.release() + '\n');
 		res.write('OS Uptime: ' + os.uptime() + '\n');
