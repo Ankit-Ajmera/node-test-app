0c732446ffcba5d389c3ae793bfea3253568237e
Ankit-Ajmera <ankit.ajmera3@gmail.com>
2018-10-09 13:43:23 +0400

    Update server.js

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ var server = http.createServer(function (req, res) {
 		res.write('OS Free memory: ' + os.freemem() / 1024 / 1024 + 'mb\n');
 		res.write('OS Total memory: ' + os.totalmem() / 1024 / 1024 + 'mb\n');
 		res.write('OS CPU count: ' + os.cpus().length + '\n');
-		res.write('OS CPU model: ' + os.cpus()[0].model + '\n');
+		res.write('OSANKIT CPU model: ' + os.cpus()[0].model + '\n');
 		res.write('OSSSSSSSSSSSSSS CPU speed: ' + os.cpus()[0].speed + 'mhz\n');
 		res.end('\n');
 
